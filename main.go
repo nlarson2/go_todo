bac91d4c0a28b9706111d72b47043eefa04977e4
nick <nlarson@users.noreply.github.com>
2023-08-07 00:15:44 -0500

    Set up db connection to Linode PSQL DB

--- main.go
+++ main.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	// "os"
-	// "io"
-	// "fmt"
-	// "net/http"
-	// "github.com/joho/godotenv"
-	// "github.com/gin-gonic/gin"
-	"database/sql"
+    "os"
+    // "io"
     "fmt"
-    // _ "github.com/lib/pq"
+    // "net/http"
+    "github.com/joho/godotenv"
+    // "github.com/gin-gonic/gin"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "log"
+    
 )
 
-func connectDB() (*sql.DB, error) {
+func connectDB( username string, password string, db_host string, db_name string, port int) (*sql.DB, error) {
     // Replace with your actual database connection string
-    connectionString := "user=nlarson password=password dbname=bible_db host=localhost port=5432 sslmode=disable"
-	fmt.Println(connectionString)
+    
+    connectionString := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable", username, password, db_name, db_host, port)
+    // fmt.Println(connectionString)
     // Open the database connection
     db, err := sql.Open("postgres", connectionString)
     if err != nil {
-        return nil, err
+	return nil, err
     }
 
     // Check if the connection is successful
     if err = db.Ping(); err != nil {
-        return nil, err
+	return nil, err
     }
 
     fmt.Println("Connected to PostgreSQL!")
@@ -33,87 +35,96 @@ func connectDB() (*sql.DB, error) {
 
 func main() {
 
-	db, err := connectDB()
+    //create log
+    f, _ := os.Create("gin.log")
+    err := godotenv.Load(".env")
     if err != nil {
-        fmt.Println("Error connecting to the database:", err)
-        return
+    	panic(err)
     }
-	fmt.Println("CONNECTED")
+
+    username := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+    dbhost := os.Getenv("DB_HOST")
+
+    db, err := connectDB(username, password, dbhost, dbname, 5432)
+    if err != nil {
+	    fmt.Println("Error connecting to the database:", err)
+	    return
+    }
+    fmt.Println("CONNECTED")
     defer db.Close()
 
-    // Query data from the database
-    rows, err := db.Query("SELECT * FROM your_table_name")
+
+    gin.DefaultWriter = io.MultiWriter(f)
+    router := gin.Default()
+
+    router.GET("/", func(c *gin.Context) {
+
+        c.SecureJSON(http.StatusOK, gin.H{
+            "message":"pong",
+        })
+    })
+
+
+    //get list of bible versions
+    //get list of books
+    //get list of chapters
+    //get range of verses
+        //limit to # of verses
+
+    router.POST("/verse/add", func(c *gin.Context) {
+        bible_name := c.PostForm("bible_name")
+        book_name := c.PostForm("book_name")
+        chapter_num := c.PostForm("chapter_num")
+        verse_num := c.PostForm("verse_num")
+        verse_text := c.PostForm("verse_text")
+
+        
+    })
+    router.POST("/verse/update", func(c *gin.Context) {
+        name := c.PostForm("name")
+        fmt.Println(name)
+    })
+    router.POST("/verse/delete", func(c *gin.Context) {
+        name := c.PostForm("name")
+        fmt.Println(name)
+    })
+
+    err = router.Run(os.Getenv("URL") + ":" + os.Getenv("PORT"))
     if err != nil {
-        fmt.Println("Error executing query:", err)
-        return
-    }
-    defer rows.Close()
-
-    // Process the query results
-    for rows.Next() {
-        var id int
-        var name string
-        var age int
-
-        if err := rows.Scan(&id, &name, &age); err != nil {
-            fmt.Println("Error scanning row:", err)
-            return
-        }
-
-        fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
-    }
-
-    if err = rows.Err(); err != nil {
-        fmt.Println("Error retrieving query results:", err)
-        return
+        panic(err)
     }
 
 
-	// //create log
-	// f, _ := os.Create("gin.log")
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// gin.DefaultWriter = io.MultiWriter(f)
-	// router := gin.Default()
-
-	// router.GET("/", func(c *gin.Context) {
-
-	// 	c.SecureJSON(http.StatusOK, gin.H{
-	// 		"message":"pong",
-	// 	})
-	// })
-
-
-	// //get list of bible versions
-	// //get list of books
-	// //get list of chapters
-	// //get range of verses
-	// 	//limit to # of verses
-
-	// router.POST("/verse/add", func(c *gin.Context) {
-	// 	bible_name := c.PostForm("bible_name")
-	// 	book_name := c.PostForm("book_name")
-	// 	chapter_num := c.PostForm("chapter_num")
-	// 	verse_num := c.PostForm("verse_num")
-	// 	verse_text := c.PostForm("verse_text")
-
-		
-
-	// })
-	// router.POST("/verse/update", func(c *gin.Context) {
-	// 	name := c.PostForm("name")
-	// 	fmt.Println(name)
-	// })
-	// router.POST("/verse/delete", func(c *gin.Context) {
-	// 	name := c.PostForm("name")
-	// 	fmt.Println(name)
-	// })
-
-	// err = router.Run(os.Getenv("URL") + ":" + os.Getenv("PORT"))
-	// if err != nil {
-	// 	panic(err)
-	// }
+   
 }
+
+
+
+    // // Query data from the database
+    // rows, err := db.Query("SELECT name FROM bibles")
+	// if err != nil {
+	// 	log.Fatal("Error executing query:", err)
+	// }
+	// defer rows.Close()
+
+	// // Store the table names in a slice
+	// var tableNames []string
+	// for rows.Next() {
+	// 	var tableName string
+	// 	if err := rows.Scan(&tableName); err != nil {
+	// 		log.Fatal("Error scanning row:", err)
+	// 	}
+	// 	tableNames = append(tableNames, tableName)
+	// }
+
+	// if err = rows.Err(); err != nil {
+	// 	log.Fatal("Error retrieving query results:", err)
+	// }
+
+	// // Print the table names
+	// fmt.Println("Table Names:")
+	// for _, tableName := range tableNames {
+	// 	fmt.Println(tableName)
+	// }
